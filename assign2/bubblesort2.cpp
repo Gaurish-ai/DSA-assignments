14d1f319f3098ed084f513ad13062b1229da267b
Gaurish garg <gaurish9d18@gmail.com>
2025-08-18 21:38:31 +0530

    Add files via upload

---
+++ assign2/bubblesort2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={8,3,4,1,0};
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<=5-i-1;j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp;
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+cout<<"sorted array is : ";
+    for(int i=0;i<5;i++)
+    {
+    cout<<arr[i]<<endl;
+    }
+}
