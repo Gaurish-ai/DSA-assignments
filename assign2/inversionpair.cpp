a6ca764189696e91d95c6de0b56032317db9450e
Gaurish garg <gaurish9d18@gmail.com>
2025-08-24 01:01:33 +0530

    Add files via upload

---
+++ assign2/inversionpair.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+ int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    int count=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(nums[i]>nums[j])
+            {
+                count++;
+            }
+        }
+    }
+    cout<<"No of inversion pairs: "<<count;
+}
